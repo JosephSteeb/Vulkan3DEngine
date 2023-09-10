d83d2608c7e9c8b64956e482714a239bb99d9e34
JosephSteeb <97857131+JosephSteeb@users.noreply.github.com>
2023-09-10 00:31:43 -0700

    added base project

---
+++ build/lib/glm/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/joest/Desktop/code/Vulkan_GameEngine/lib/glm
+# Build directory: C:/Users/joest/Desktop/code/Vulkan_GameEngine/build/lib/glm
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("glm")
---
+++ build/lib/glm/glm/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/joest/Desktop/code/Vulkan_GameEngine/lib/glm/glm
+# Build directory: C:/Users/joest/Desktop/code/Vulkan_GameEngine/build/lib/glm/glm
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
