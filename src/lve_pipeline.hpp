4de8db0dbde66ceb0daab33a75a11f5dad2f42df
JosephSteeb <97857131+JosephSteeb@users.noreply.github.com>
2023-09-10 21:10:31 -0700

    added device code

--- src/lve_pipeline.hpp
+++ src/lve_pipeline.hpp
@@ -1,16 +1,42 @@
 #pragma once
+
+#include "lve_device.hpp"
+
+// std
 #include <string>
 #include <vector>
 
 namespace lve
 {
+    struct PipelineConfigInfo
+    {
+    };
     class LvePipeline
     {
     public:
-        LvePipeline(const std::string &vertexFilepath, const std::string &fragFilepath);
+        LvePipeline(
+            LveDevice &device,
+            const std::string &vertexFilepath,
+            const std::string &fragFilepath,
+            const PipelineConfigInfo configInfo);
+        ~LvePipeline() {}
+        LvePipeline(const LvePipeline &) = delete;
+        void operator=(const LvePipeline &) = delete;
+
+        static PipelineConfigInfo defaultPipelineConfigInfo(uint32_t width, uint32_t height);
 
     private:
         static std::vector<char> readFile(const std::string &f);
-        void createGraphicsPipeline(const std::string &vertFilepath, const std::string &fragFilepath);
+        void createGraphicsPipeline(
+            const std::string &vertFilepath,
+            const std::string &fragFilepath,
+            const PipelineConfigInfo &configInfo);
+
+        void createShaderModule(const std::vector<char> &code, VkShaderModule *VkShaderModule);
+
+        LveDevice &lveDevice;
+        VkPipeline graphicsPipeline;
+        VkShaderModule vertShaderModule;
+        VkShaderModule fragShaderModule;
     };
 }
